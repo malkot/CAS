3b2fa0fa261e6421b0c3580ec8c7589c7ff29e3b
malkot <ivan___1988@mail.ru>
2016-03-21 19:51:04 +0300

    commit

---
+++ WindowsService2/COM.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Threading;
+using System.IO;
+
+namespace WindowsService2
+{
+    class COM
+    {
+        System.IO.Ports.SerialPort rs_port;
+        public string num_COM;
+        public byte ID;
+        public void Inic_COM()
+        {
+            rs_port = new System.IO.Ports.SerialPort(num_COM, 9600,                    //Настройка COM-порта {Скорость - 9600, Чётность - НЕТ, Бит данных - 8, Стоповый бит - 1}
+            System.IO.Ports.Parity.None, 8,
+            System.IO.Ports.StopBits.One);
+
+            if (rs_port.IsOpen == true)
+            {
+                rs_port.Close();
+            }
+            rs_port.Open();
+        }
+
+        public void Opros()
+        {                                                                                    
+            StreamReader file = new StreamReader(@"c:\ConnectionString.txt");
+            string str_conn = file.ReadLine();
+            SqlConnection conn = new SqlConnection(str_conn);                                         //Connection String
+            byte[] data = new byte[7] { 0x44, 0x30, ID, 0x4B, 0x57, 0x0D, 0x0A };                     //Посылка согласно протокола
+            rs_port.Write(data, 0, 7);                                                                //Запись посылки в порт
+            System.Threading.Thread.Sleep(100);                                                       //Задержка, ожидание ответа от прибора            
+            string readmsg;
+
+            if (rs_port.BytesToRead == 22)                                                            //Определение длины посылки
+            {
+                byte[] answer = new byte[(int)rs_port.BytesToRead];                                   //Массив для ответа от прибора
+                rs_port.Read(answer, 0, rs_port.BytesToRead);
+                readmsg = Encoding.ASCII.GetString(answer);                                           //Перекодировка отвера в ASCII
+                string stab, num, weight, kg;               
+                stab = (readmsg.Remove(2));                                                           //Отделение признака стабильности из общей посылки 
+
+                if (String.Equals(stab, "ST", StringComparison.CurrentCulture))                       //Если вес стабилен - идём проверять значение веса
+                {
+                    num = (readmsg.Remove(0, 6));                                                     //Отделение номера прибора из общей посылки 
+                    num = (num.Remove(1));
+                    weight = (readmsg.Remove(0, 10));                                                 //Отделение значения веса из общей посылки 
+                    weight = (weight.Remove(7));
+                    weight = weight.Replace(" ", "");
+                    weight = weight.Replace(".", ",");
+                    double weight_double = System.Convert.ToDouble(weight);                           //Конвертация значения веса из String в Double
+                    if (weight_double > 15)                                                           //Если вес больше 15кг пишем в базу и тормозим поток на 10 сек.
+                    {
+                        kg = (readmsg.Remove(0, 18));
+                        kg = (kg.Remove(2));
+                        DateTime date;
+                        date = DateTime.Now;
+                        conn.Open();
+                        string Insquery = "INSERT INTO Arhiv (date,value,id) VALUES ('" + date + "','" + weight_double + "', '" + num + "')";
+                        SqlCommand cmd = new SqlCommand(Insquery, conn);
+                        cmd.ExecuteNonQuery();
+                        conn.Close();
+                        Thread.Sleep(10000);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WindowsService2/Service1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace WindowsService2
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string[] res = System.IO.File.ReadAllLines(@"c:\ConnectionString.txt");
+            int kol_devices = (res.Length - 1);            // Количество приборов
+            Thread[] threads = new Thread[kol_devices];    // Объявление массива потоков в количестве равном количеству приборов
+
+            for (int i = 0; i < kol_devices; i++)          // Цикл для создания потоков
+            {
+                threads[i] = new Thread(Opros);            // создание потоков
+                threads[i].Name = "Potok " + i.ToString(); // Имя потока
+                threads[i].IsBackground = true;            // Свойство потока - фоновый
+                threads[i].Start();                        // Запуск потока
+            }                      
+        }
+
+        private static void Opros()
+        {
+
+            string[] res = System.IO.File.ReadAllLines(@"c:\ConnectionString.txt");
+            COM COM1 = new COM();                           // Объявление объекта класса 'COM'
+            COM1.num_COM = res[1];                          // Применение полю 'num_COM' значения 'COM1'
+            COM1.ID = 0x31;                                 // Применение полю ID значения "1"
+            COM1.Inic_COM();                                // Инициализация COM порта
+
+            while (true)
+            {
+                COM1.Opros();                               // Запуска метода 'Opros'
+                Thread.Sleep(100);                          // Задержка 0.1сек
+            }
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
