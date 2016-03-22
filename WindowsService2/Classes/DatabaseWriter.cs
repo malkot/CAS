453b5c604970b67ecd94ab90431de437ee4c24a1
Vladimir <riuson@gmail.com>
2016-03-22 21:30:15 +0500

    Added class to save values to database

---
+++ WindowsService2/Classes/DatabaseWriter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService2.Classes
+{
+    internal class DatabaseWriter
+    {
+        private string mConnectionString;
+
+        public DatabaseWriter()
+        {
+            this.mConnectionString = String.Empty;
+        }
+
+        public void Configure(string connectionString)
+        {
+            this.mConnectionString = connectionString;
+        }
+
+        public void SaveValue(int deviceId, double weightValue)
+        {
+            using (SqlConnection connection = new SqlConnection(this.mConnectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("insert into Arhiv (date, value, id) values (@date, @value, @id);"))
+                {
+                    command.Parameters.Add(new SqlParameter()
+                    {
+                        ParameterName = "@date",
+                        Value = DateTime.Now,
+                        SqlDbType = System.Data.SqlDbType.DateTime
+                    });
+                    command.Parameters.Add(new SqlParameter()
+                    {
+                        ParameterName = "@value",
+                        Value = weightValue,
+                        SqlDbType = System.Data.SqlDbType.Float
+                    });
+                    command.Parameters.Add(new SqlParameter()
+                    {
+                        ParameterName = "@id",
+                        Value = deviceId,
+                        SqlDbType = System.Data.SqlDbType.Int
+                    });
+
+                    int insertedRecords = command.ExecuteNonQuery();
+                }
+
+                connection.Close();
+            }
+        }
+    }
+}
